925525325d8b420f766e6ba10a51a3088075c4b3
ibra-kdbra <git-repo-develop@outlook.com>
2026-01-20 00:53:18 +0300

    moved

---
+++ scripts/run.sh
@@ -0,0 +1,322 @@
+#!/bin/bash
+
+function run_ruby {
+    echo "*------- ruby ---------*"
+    
+    gem update --system
+    gem install cmath
+    ruby  -v
+
+    cd ./src/benchmarks
+    ruby  ./ruby.rb
+
+    sleep 5 # cpu cool down
+    ./src/benchmarks/prime
+}
+
+function run_assembly {
+    echo "*------- assembly (nasm) ---------*"
+    nasm --version
+
+    cd ./src/benchmarks
+    nasm -f elf64 ./prime.asm
+    ld -s -o prime prime.o
+    cd ../..
+
+    sleep 5 # cpu cool down
+    ./src/benchmarks/prime
+
+    # check if file exists before attempting to remove it
+    [ -f "./src/benchmarks/prime.o" ] && rm ./src/benchmarks/prime.o
+    [ -f "./src/benchmarks/prime" ] && rm ./src/benchmarks/prime
+}
+
+function run_c_native {
+    echo "*---- c native (opt-level=3) -----*"
+    gcc --version | head -n 1
+    gcc ./src/benchmarks/prime.c -o prime -lm -g -O3
+
+    sleep 5 # cpu cool down
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+}
+
+function run_cpp_native {
+    echo "*--- c++ native (opt-level=3) ----*"
+    g++ --version | head -n 1
+    g++ ./src/benchmarks/prime.cpp -o prime -g -O3
+
+    sleep 5 # cpu cool down
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+}
+
+function run_rust_native {
+    echo "*--- rust native (opt-level=3) ---*"
+    rustc --version
+    rustc -C opt-level=3 -C target-cpu=native -C codegen-units=1 -C lto -C overflow-checks=off ./src/benchmarks/prime.rs
+
+    sleep 5 # cool down cpu
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+}
+
+function run_julia {
+    echo "*------------- julia -------------*"
+    julia --version
+
+    sleep 5 # cpu cool down
+
+    julia ./src/benchmarks/prime.jl
+    echo ""
+    echo ""
+}
+
+function run_go {
+    echo "*-------------- go ---------------*"
+    go version
+    go build -ldflags "-s -w" ./src/benchmarks/prime.go
+
+    sleep 5 # cpu cool down
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+    echo ""
+}
+
+function run_java {
+    echo "*-------- java (open-jdk) --------*"
+    javac --version
+    javac ./src/benchmarks/Prime.java
+
+    sleep 5 # cpu cool down
+
+    cd src/benchmarks
+    java Prime
+    # check if file exists before attempting to remove it
+    [ -f "./Prime.class" ] && rm ./Prime.class
+    cd ../..
+    echo ""
+}
+
+function run_nodejs {
+    echo "*------------ nodejs -------------*"
+    node --version
+
+    sleep 5 # cool down cpu
+
+    node ./src/benchmarks/prime.js
+    echo ""
+}
+
+function run_csharp_mono {
+    echo "*------------ c# Mono ------------*"
+    mcs --version
+    mcs ./src/benchmarks/prime.cs
+
+    sleep 5 # cpu cool down
+
+    mono ./src/benchmarks/prime.exe
+    # check if file exists before attempting to remove it
+    [ -f "./src/benchmarks/prime.exe" ] && rm ./src/benchmarks/prime.exe
+    echo ""
+}
+
+function run_dart_native {
+    echo "*---------- dart native ----------*"
+    dart --version
+    dart compile exe ./src/benchmarks/prime.dart
+
+    sleep 5 # cpu cool down
+
+    ./src/benchmarks/prime.exe
+    # check if file exists before attempting to remove it
+    [ -f "./src/benchmarks/prime.exe" ] && rm ./src/benchmarks/prime.exe
+    echo ""
+}
+
+function run_python_codon {
+    echo "*--------- python codon ----------*"
+    codon --version
+    codon build -release -exe ./src/benchmarks/prime.py
+
+    sleep 5 # cpu cool down
+
+    ./src/benchmarks/prime
+    # check if file exists before attempting to remove it
+    [ -f "./src/benchmarks/prime" ] && rm ./src/benchmarks/prime
+    echo ""
+}
+
+function run_pascal {
+    echo "*------------ pascal -------------*"
+    fpc ./src/benchmarks/prime.pas
+
+    sleep 5 # cpu cool down
+
+    ./src/benchmarks/prime
+    # check if file exists before attempting to remove it
+    [ -f "./src/benchmarks/prime.o" ] && rm ./src/benchmarks/prime.o
+    [ -f "./src/benchmarks/prime" ] && rm ./src/benchmarks/prime
+    echo ""
+}
+
+function run_python {
+    echo "*------------ python -------------*"
+    python3 --version
+
+    sleep 5 # cpu cool down
+
+    sleep 5 # cpu cool down
+    python3 ./src/benchmarks/prime.py
+    echo ""
+}
+
+function run_php {
+    echo "*------------ Php -------------*"
+    php --version
+
+    sleep 5 # cpu cool down
+
+    php ./src/benchmarks/prime.php
+    echo ""
+}
+
+function run_r {
+    echo "*------------ R -------------*"
+    Rscript --version
+
+    sleep 5 # cpu cool down
+
+    Rscript ./src/benchmarks/prime.R
+    echo ""
+}
+
+function run_haskell {
+    echo "*----------- Haskell -----------*"
+    ghc --version
+    ghc -O3 -dynamic ./src/benchmarks/prime.hs -o prime_hs
+
+    sleep 5 # cpu cool down
+
+    ./prime_hs
+    # check if file exists before attempting to remove it
+    [ -f "./prime_hs" ] && rm ./prime_hs
+    [ -f "./src/benchmarks/prime.hi" ] && rm ./src/benchmarks/prime.hi
+    [ -f "./src/benchmarks/prime.o" ] && rm ./src/benchmarks/prime.o
+    echo ""
+}
+
+# Define an associative array to map language names to function names
+declare -A langs=(
+    ["assembly"]="run_assembly"
+    ["c"]="run_c_native"
+    ["cpp"]="run_cpp_native"
+    ["rust"]="run_rust_native"
+    ["go"]="run_go"
+    ["julia"]="run_julia"
+    ["java"]="run_java"
+    ["nodejs"]="run_nodejs"
+    ["csharp_mono"]="run_csharp_mono"
+    ["dart"]="run_dart_native"
+    ["python_codon"]="run_python_codon"
+    ["pascal"]="run_pascal"
+    ["python"]="run_python"
+    ["php"]="run_php"
+    ["r"]="run_r"
+    ["ruby"]="run_ruby"
+    ["haskell"]="run_haskell"
+    ["zig"]="run_zig"
+    ["fortran"]="run_fortran"
+    ["nim"]="run_nim"
+)
+
+function run_zig {
+    echo "*------------ Zig -------------*"
+    zig version
+    zig build-exe ./src/benchmarks/prime.zig -O ReleaseFast --name prime
+
+    sleep 5 # cpu cool down
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+}
+
+function run_fortran {
+    echo "*----------- Fortran -----------*"
+    gfortran --version | head -n 1
+    gfortran -O3 ./src/benchmarks/prime.f90 -o prime
+
+    sleep 5 # cpu cool down
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+}
+
+function run_nim {
+    echo "*------------- Nim -------------*"
+    nim --version | head -n 1
+    nim c -d:release --hints:off ./src/benchmarks/prime.nim
+    mv ./src/benchmarks/prime ./prime
+
+    sleep 5 # cpu cool down
+
+    ./prime
+    # check if file exists before attempting to remove it
+    [ -f "./prime" ] && rm ./prime
+    echo ""
+}
+
+# Define a function to display the help message
+function display_help {
+    echo "Usage: $0 [LANGUAGE]"
+    echo ""
+    echo "Run code in various programming languages."
+    echo ""
+    echo "If no argument is provided, the script will run code in all supported"
+    echo "languages. Otherwise, provide a language name to run the corresponding"
+    echo "code. Supported languages are:"
+    echo ""
+    for lang in "${!langs[@]}"; do
+        echo "- $lang"
+    done
+    echo ""
+    echo "Examples:"
+    echo "  # Run code in all supported languages"
+    echo "  $0"
+    echo ""
+    echo "  # Run C++ code"
+    echo "  $0 cpp"
+}
+
+if [ $# -eq 0 ]; then
+    # Loop through all languages and call their corresponding functions
+    for lang in "${!langs[@]}"; do
+        ${langs["$lang"]}
+    done
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    # Display the help message if requested by the user
+    display_help
+else
+    # Check if the input argument is a valid language name and call its corresponding function
+    if [[ ${langs[$1]+_} ]]; then
+        ${langs[$1]}
+    else
+        echo "Invalid choice"
+    fi
+fi
